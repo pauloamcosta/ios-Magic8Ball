7b635a9cd4f2ab2b4d85dcc0f94831857475ab40
Paulo Andre Melo Costa <pauloamcosta2@gmail.comgit>
2018-10-28 01:52:40 -0300

    Initial Commit

---
+++ Magic 8 Ball./ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball.
+//
+//  Created by Paulo Costa on 28/10/18.
+//  Copyright © 2018 Paulo Costa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
